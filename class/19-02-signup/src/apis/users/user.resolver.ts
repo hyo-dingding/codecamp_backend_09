2572f9a1aab715b4d450acdc5c433d861bdec7ff
hyo-dingding <zvoniti2964@gmail.com>
2022-09-28 02:20:58 +0900

    20일차class

---
+++ class/19-02-signup/src/apis/users/user.resolver.ts
@@ -0,0 +1,19 @@
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './user.service';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+  @Mutation(() => User)
+  createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ) {
+    return this.usersService.create({ email, password, name, age });
+  }
+}
